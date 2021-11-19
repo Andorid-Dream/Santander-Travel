298f70449d36524d3f28b3bef85475ee6d980663
Eider22 <eidercardona22@gmail.com>
2021-11-19 14:34:48 -0500

    Se creó la primera versión del card view Se creó la actividad "activity_list_poi" y se definió como la actividad que se lanzará al abrir la app

---
+++ app/src/main/java/com/eidev/santandertravel/ListPoiActivity.kt
@@ -0,0 +1,11 @@
+package com.eidev.santandertravel
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ListPoiActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_list_poi)
+    }
+}
