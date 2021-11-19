a37dc60268bd480976dbcd894d26c0f5c4858375
Eider22 <eidercardona22@gmail.com>
2021-11-19 17:22:52 -0500

    Se creó el recyclerView. Este se hizo quemando datos de una lista de objetos de tipo Poi. Se mejoró el card view. Se hizo uso de la librería Picasso para llamar las imágenes al recycler view

--- app/src/main/java/com/eidev/santandertravel/ListPoiActivity.kt
+++ app/src/main/java/com/eidev/santandertravel/ListPoiActivity.kt
@@ -1,11 +1,118 @@
 package com.eidev.santandertravel
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class ListPoiActivity : AppCompatActivity() {
+
+    private lateinit var poiList: ArrayList<Poi>
+    private lateinit var poisAdapter: PoisAdapter
+    private lateinit var poisRecyclerView: RecyclerView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_list_poi)
+
+        poisRecyclerView = findViewById(R.id.pois_recycler_view)
+        poiList = createMockupPois()
+        poisAdapter = PoisAdapter(poiList)
+        poisRecyclerView.apply {
+            layoutManager = LinearLayoutManager(context)
+            adapter = poisAdapter
+            setHasFixedSize(false)
+        }
+
+    }
+
+    private fun createMockupPois(): ArrayList<Poi> {
+        return arrayListOf(
+            Poi(
+                name = "test",
+                shortDetail = "test",
+                punctuation = 5,
+                urlPicture = "https://www.vanguardia.com/binrepository/716x477/0c0/0d0/none/12204/ITOC/web_ecolo001_big_ce_VL142422_MG22138284.jpg"
+            ),
+            Poi(
+                name = "test",
+                shortDetail = "test",
+                punctuation = 5,
+                urlPicture = "https://www.vanguardia.com/binrepository/716x477/0c0/0d0/none/12204/ITOC/web_ecolo001_big_ce_VL142422_MG22138284.jpg"
+            ),
+            Poi(
+                name = "test",
+                shortDetail = "test",
+                punctuation = 5,
+                urlPicture = "https://www.vanguardia.com/binrepository/716x477/0c0/0d0/none/12204/ITOC/web_ecolo001_big_ce_VL142422_MG22138284.jpg"
+            ),
+            Poi(
+                name = "test",
+                shortDetail = "test",
+                punctuation = 5,
+                urlPicture = "https://www.vanguardia.com/binrepository/716x477/0c0/0d0/none/12204/ITOC/web_ecolo001_big_ce_VL142422_MG22138284.jpg"
+            ),
+            Poi(
+                name = "test",
+                shortDetail = "test",
+                punctuation = 5,
+                urlPicture = "https://www.vanguardia.com/binrepository/716x477/0c0/0d0/none/12204/ITOC/web_ecolo001_big_ce_VL142422_MG22138284.jpg"
+            ),
+            Poi(
+                name = "test",
+                shortDetail = "test",
+                punctuation = 5,
+                urlPicture = "https://www.vanguardia.com/binrepository/716x477/0c0/0d0/none/12204/ITOC/web_ecolo001_big_ce_VL142422_MG22138284.jpg"
+            ),
+            Poi(
+                name = "test",
+                shortDetail = "test",
+                punctuation = 5,
+                urlPicture = "https://www.vanguardia.com/binrepository/716x477/0c0/0d0/none/12204/ITOC/web_ecolo001_big_ce_VL142422_MG22138284.jpg"
+            ),
+            Poi(
+                name = "test",
+                shortDetail = "test",
+                punctuation = 5,
+                urlPicture = "https://www.vanguardia.com/binrepository/716x477/0c0/0d0/none/12204/ITOC/web_ecolo001_big_ce_VL142422_MG22138284.jpg"
+            ),
+            Poi(
+                name = "test",
+                shortDetail = "test",
+                punctuation = 5,
+                urlPicture = "https://www.vanguardia.com/binrepository/716x477/0c0/0d0/none/12204/ITOC/web_ecolo001_big_ce_VL142422_MG22138284.jpg"
+            ),
+            Poi(
+                name = "test",
+                shortDetail = "test",
+                punctuation = 5,
+                urlPicture = "https://www.vanguardia.com/binrepository/716x477/0c0/0d0/none/12204/ITOC/web_ecolo001_big_ce_VL142422_MG22138284.jpg"
+            ),
+            Poi(
+                name = "test",
+                shortDetail = "test",
+                punctuation = 5,
+                urlPicture = "https://www.vanguardia.com/binrepository/716x477/0c0/0d0/none/12204/ITOC/web_ecolo001_big_ce_VL142422_MG22138284.jpg"
+            ),
+            Poi(
+                name = "test",
+                shortDetail = "test",
+                punctuation = 5,
+                urlPicture = "https://www.vanguardia.com/binrepository/716x477/0c0/0d0/none/12204/ITOC/web_ecolo001_big_ce_VL142422_MG22138284.jpg"
+            ),
+            Poi(
+                name = "test",
+                shortDetail = "test",
+                punctuation = 5,
+                urlPicture = "https://www.vanguardia.com/binrepository/716x477/0c0/0d0/none/12204/ITOC/web_ecolo001_big_ce_VL142422_MG22138284.jpg"
+            ),
+            Poi(
+                name = "test",
+                shortDetail = "test",
+                punctuation = 5,
+                urlPicture = "https://www.vanguardia.com/binrepository/716x477/0c0/0d0/none/12204/ITOC/web_ecolo001_big_ce_VL142422_MG22138284.jpg"
+            )
+
+        )
     }
 }
