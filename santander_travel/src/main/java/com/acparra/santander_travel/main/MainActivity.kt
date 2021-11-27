1a40d45a56f7be9ba5ba35a8cbeb032d4ae67fae
lore0523 <lorena.arq1@gmail.com>
2021-11-27 10:20:04 -0500

    Nueva rama feature/conection-test

--- santander_travel/src/main/java/com/acparra/santander_travel/main/MainActivity.kt
+++ santander_travel/src/main/java/com/acparra/santander_travel/main/MainActivity.kt
@@ -2,6 +2,11 @@ package com.acparra.santander_travel.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
 import com.acparra.santander_travel.R
 
 class MainActivity : AppCompatActivity() {
@@ -9,4 +14,30 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+       val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.menu_overflow, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val fm: FragmentManager = supportFragmentManager
+        val ft: FragmentTransaction= fm.beginTransaction()
+
+        return when (item.itemId){
+            R.id.menu_preferences ->{
+                val fragmentSettings = SettingsFragment()
+                ft.replace(R.id.fragmentContainerView, fragmentSettings).commit()
+                true
+            }
+            else -> {return true}
+        }
+    }
+
+
+
+
+
+
 }
