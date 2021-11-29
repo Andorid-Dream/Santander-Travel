35bdab7f827b989eeee2032b91ac6e8ef807e336
lore0523 <lorena.arq1@gmail.com>
2021-11-29 13:33:56 -0500

    opciones 2

--- santander_travel/src/main/java/com/acparra/santander_travel/list/ListFragment.kt
+++ santander_travel/src/main/java/com/acparra/santander_travel/list/ListFragment.kt
@@ -8,12 +8,12 @@ import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.acparra.santander_travel.databinding.FragmentListBinding
+import com.acparra.santander_travel.main.MainActivity
 import com.acparra.santander_travel.model.Poi
 import com.acparra.santander_travel.model.PoiItem
 import com.google.gson.Gson
 
 class ListFragment : Fragment() {
-
     private lateinit var listBinding: FragmentListBinding
     private lateinit var poisAdapter: PoisAdapter
     private lateinit var listPois: ArrayList<PoiItem>
@@ -21,19 +21,18 @@ class ListFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         listBinding = FragmentListBinding.inflate(inflater, container, false)
 
         return listBinding.root
     }
 
-
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-        listPois = loadPoisFromJSON()
-
+        (activity as MainActivity?)?.hideIcon()
+        listPois = loadMockPoisFromJSON()
         poisAdapter = PoisAdapter(listPois, onItemClicked = {onPoiClicked(it)})
+
         listBinding.poisRecyclerView.apply {
             layoutManager = LinearLayoutManager(context)
             adapter = poisAdapter
@@ -41,17 +40,18 @@ class ListFragment : Fragment() {
         }
     }
 
-    private fun loadPoisFromJSON(): ArrayList<PoiItem> {
 
-        val poisString = context?.assets?.open("ciudades.json")?.bufferedReader().use {it?.readText()}
-        val gson = Gson()
-        val info = gson.fromJson(poisString, Poi::class.java)
-
-        return info
-    }
 
     private fun onPoiClicked(poi: PoiItem){
-        findNavController().navigate(ListFragmentDirections.actionListFragmentToDetailFragment(poi = poi))
-        //findNavController().navigate(ListFragmentDirections.actionNavigationListToSettingsFragment())
+       // findNavController().navigate(ListFragmentDirections.actionListFragmentToDetailFragment(poi = poi))
+       findNavController().navigate(ListFragmentDirections.actionNavigationListToSettingsFragment())
     }
+
+    private fun loadMockPoisFromJSON(): ArrayList<PoiItem> {
+        val poisString: String = context?.assets?.open("ciudades.json")?.bufferedReader().use {it!!.readText()}
+        val gson = Gson()
+        val poisList = gson.fromJson(poisString, Poi::class.java)
+        return poisList
+    }
+
 }
