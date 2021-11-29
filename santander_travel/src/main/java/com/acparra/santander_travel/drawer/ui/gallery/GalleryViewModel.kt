c7b2a86bd4d4cde64473e74b821d3b9c72b644e8
alejandro <alejomv21@gmail.com>
2021-11-28 23:35:01 -0500

    tercero

---
+++ santander_travel/src/main/java/com/acparra/santander_travel/drawer/ui/gallery/GalleryViewModel.kt
@@ -0,0 +1,13 @@
+package com.acparra.santander_travel.drawer.ui.gallery
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class GalleryViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is gallery Fragment"
+    }
+    val text: LiveData<String> = _text
+}
